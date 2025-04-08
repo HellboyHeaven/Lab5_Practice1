7c5319b5839a6ec5adbca08fd220324d48745919
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2025-04-08 21:52:17 +0200

    inital

--- MultithreadingAndAsynchronousTutorial/MultithreadingTutorial/Program.cs
+++ MultithreadingAndAsynchronousTutorial/MultithreadingTutorial/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello multithreading!");
+﻿var counter = 0;
+var dummy = new Object();
+
+var t1 = Task.Run(Increment);
+var t2 = Task.Run(Increment);
+
+Task.WaitAll(t1, t2);
+
+Console.WriteLine($"The value of counter: {counter}");
+
+void Increment()
+{
+    for (int i = 0; i < 1_000_000_000; i++)
+    {
+        if (i > 0 && i % 100_000_000 == 0)
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {i} was reached");
+        lock (dummy)
+        {
+            counter++;
+        }
+    }
+
+    Console.WriteLine($"{Thread.CurrentThread.Name}: The job is completed ");
+}
+
--- MultithreadingAndAsynchronousTutorial/WeatherApp/Form1.cs
+++ MultithreadingAndAsynchronousTutorial/WeatherApp/Form1.cs
@@ -1,10 +1,15 @@
+using System.Security.Policy;
+
 namespace TutorialAsyncAwait
 {
     public partial class Form1 : Form
     {
+        private const string URL = "https://weatherslowapi.azurewebsites.net/weather/";
+        private readonly HttpClient _httpClient = new HttpClient();
         public Form1()
         {
             InitializeComponent();
+            _httpClient.BaseAddress = new Uri(URL);
         }
 
         private void dummyButton1_Click(object sender, EventArgs e)
@@ -15,6 +20,29 @@ namespace TutorialAsyncAwait
         private void dummyButton2_Click(object sender, EventArgs e)
         {
             dummyTextBox.Text = "Even more dummy text...";
+        }
+
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Clear();
+            var cities = citiesTextBox.Text.Split(",");
+            var tasks = new List<Task<string>>();
+            foreach (var city in cities)
+            {
+                tasks.Add(_httpClient.GetStringAsync(city));
+            }
+
+            await Task.WhenAll(tasks);
+
+            while (tasks.Count > 0)
+            {
+                var finished = await Task.WhenAny(tasks);
+                tasks.Remove(finished);
+                var result = await finished;
+                resultTextBox.AppendText(result + "\r\n");
+            }
+
+            
         }
     }
 }
